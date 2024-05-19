2cff4bb0c185a0136ebbfc698bdeb53439d1482a
Anderson OSJr <anderosj@gmail.com>
2024-05-19 19:06:32 -0300

    Formated Date/Time, Status and Priority Components

--- app/tickets/DataTable.tsx
+++ app/tickets/DataTable.tsx
@@ -1,3 +1,5 @@
+import TicketPriority from "@/components/TicketPriority";
+import TicketStatusBadge from "@/components/TicketStatusBadge";
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
 import { Ticket } from "@prisma/client";
 
@@ -7,33 +9,63 @@ interface Props {
 }
 
 const DataTable = ({tickets}:Props) => {
-    console.log("Hello World!!");
-  return (
-    <div className="w-full mt-5">
-        <div className="rounded-md sm:border">
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Title</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead>Priority</TableHead>
-                        <TableHead>Created At</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    {tickets ? tickets.map((ticket) => (
-                        <TableRow key={ticket.id} data-href="/">
-                            <TableCell>{ticket.title}</TableCell>
-                            <TableCell>{ticket.status}</TableCell>
-                            <TableCell>{ticket.priority}</TableCell>
-                            <TableCell>{ticket.createdAt.toLocaleDateString()}</TableCell>
+    return (
+        <div className="w-full mt-5">
+            <div className="rounded-md sm:border">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Title</TableHead>
+                            <TableHead>
+                                <div className="flex justify-center">
+                                    Status
+                                </div>                                
+                            </TableHead>
+                            <TableHead>
+                                <div className="flex justify-center">
+                                    Priority
+                                </div>
+                            </TableHead>
+                            <TableHead>
+                                <div className="flex justify-center">
+                                    Created At
+                                </div>
+                            </TableHead>
                         </TableRow>
-                    )) : null}
-                </TableBody>
-            </Table>
+                    </TableHeader>
+                    <TableBody>
+                        {tickets ? tickets.map((ticket) => (
+                            <TableRow key={ticket.id} data-href="/">
+                                <TableCell>{ticket.title}</TableCell>
+                                <TableCell>
+                                    <div className="flex justify-center">
+                                        <TicketStatusBadge status={ticket.status}/>
+                                    </div>                                    
+                                </TableCell>
+                                <TableCell>
+                                    <div className="flex justify-center">
+                                        <TicketPriority priority={ticket.priority}/>
+                                    </div>                                    
+                                </TableCell>
+                                <TableCell>
+                                    <div className="flex justify-center">
+                                        {ticket.createdAt.toLocaleDateString("pt-BR", {
+                                            day: "2-digit",
+                                            month: "2-digit",
+                                            year: "2-digit",
+                                            hour: "numeric",
+                                            minute: "2-digit",
+                                            hour12: false,
+                                        })}
+                                    </div>
+                                </TableCell>
+                            </TableRow>
+                        )) : null}
+                    </TableBody>
+                </Table>
+            </div>
         </div>
-    </div>
-  );
+    );
 }
 
 export default DataTable;
